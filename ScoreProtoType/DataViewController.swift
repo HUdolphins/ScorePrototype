a9b3c603bcf996f52d6d5ca1ee61e35d4df69d47
fathy000 <caxki000@gmail.com>
2018-09-05 19:52:35 +0900

    dataViewController ブランチテスト

---
+++ ScoreProtoType/DataViewController.swift
@@ -0,0 +1,37 @@
+//
+//  DataViewController.swift
+//  ScoreProtoType
+//
+//  Created by Kazuki Ohashi on 2018/09/05.
+//  Copyright © 2018年 Kazuki Ohashi. All rights reserved.
+//
+
+import UIKit
+import SpreadsheetView
+
+
+class DataViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
